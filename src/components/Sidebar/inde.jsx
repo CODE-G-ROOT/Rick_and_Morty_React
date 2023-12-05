ce79948aeb415ec0cf752ecdb16243bdeb6d5885
CODE-G-ROOT <juandavidavilaravelo856@gmail.com>
2023-12-05 11:53:25 -0500

    feat: :sparkles: Componente SideBar
    Se ha creado este componente y además se ha configurado tailwind con las configuraciónes de los colores, fonts, etc.

---
+++ src/components/Sidebar/inde.jsx
@@ -0,0 +1,49 @@
+import * as f_a_icons from 'react-icons/fa'
+import { NavLink } from 'react-router-dom';
+
+const items = [
+    "Inicio",
+    "Libros",
+    "Editores",
+    "Lectores"
+];
+
+const activeStyle = 'text-white-50'
+
+const item = items.map((item) => {
+    return (
+        <li className='font-Red_Hat font-medium text-whiteTransparent '>
+            <NavLink
+                to={`/${item}`}
+                className={({ isActive }) => isActive ? activeStyle : undefined}
+            >
+                {item}
+            </NavLink>
+        </li>
+    )
+});
+
+const Side_bar = () => {
+
+    return (
+        <>
+            <div className="bg-primario h-screen w-full flex flex-col justify-around items-center">
+                <div className='flex justify-center items-center w-16 h-16 rounded-lg bg-primario_transparet shadow-lg shadow-cyan-500/50 '>
+                    <f_a_icons.FaBook className='book_icon w-10 h-10 z-10' />
+                </div>
+                <div className="">
+                    <ul className='flex flex-col text-center gap-5'>
+                        {item}
+                    </ul>
+                </div>
+                <button className='flex gap-4 justify-center items-center text-white-50 bg-primario_transparet w-2/12 h-10 rounded-lg shadow-lg shadow-cyan-500/50'>
+                    <div> Crear </div>
+                    <div className='font-extrabold text-xl'>+</div>
+                </button>
+
+            </div>
+        </>
+    )
+};
+
+export default Side_bar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
-const {nextui} = require("@nextui-org/react");
+const colors = require('tailwindcss/colors')
+const { nextui } = require("@nextui-org/react");
 
 export default {
   content: [
@@ -11,8 +12,31 @@ export default {
   theme: {
     colors: {
       primario: "#7DA870",
+      primario_transparet: "#90b687",
       secundario: "#E9A763",
-      terciario: "#343A3F"
+      terciario: "#343A3F",
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: {
+        50: "#f8fafc",
+        100: "#f1f5f9",
+        200: "#e2e8f0",
+        300: "#cbd5e1",
+        400: "#94a3b8",
+        500: "#64748b",
+        600: "#475569",
+        700: "#334155",
+        800: "#1e293b",
+        900: "#0f172a",
+        950: "#020617"
+      },
+      whiteTransparent: "#d2ddc1",
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+
     },
     fontFamily: {
       Red_Hat: ["Red Hat Display"]
