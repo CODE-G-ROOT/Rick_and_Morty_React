9d8dce601fd45f2410f4cce266bb11df4470b0b4
CODE-G-ROOT <juandavidavilaravelo856@gmail.com>
2023-12-05 13:51:52 -0500

    feat: :adhesive_bandage: correxión de colores y background
    se corrigieron los algunos colores implementados para la visualización del componente, además se implementó el componente en home

--- src/components/Sidebar/inde.jsx
+++ src/components/Sidebar/inde.jsx
@@ -30,7 +30,7 @@ const Side_bar = () => {
 
     return (
         <>
-            <div className="bg-primario h-screen w-full flex flex-col justify-around items-center">
+            <div className="w-2/12 h-full flex flex-col justify-around items-center">
                 <div className='flex justify-center items-center w-16 h-16 rounded-lg bg-primario_transparet shadow-lg shadow-cyan-500/50 '>
                     <f_a_icons.FaBook className='book_icon w-10 h-10 z-10' />
                 </div>
@@ -39,7 +39,7 @@ const Side_bar = () => {
                         {item}
                     </ul>
                 </div>
-                <button className='flex gap-4 justify-center items-center text-white-50 bg-primario_transparet w-2/12 h-10 rounded-lg shadow-lg shadow-cyan-500/50'>
+                <button className='flex gap-4 justify-center items-center text-white-50 bg-primario_transparet w-2/5 h-10 rounded-lg shadow-lg shadow-cyan-500/50'>
                     <div> Crear </div>
                     <div className='font-extrabold text-xl'>+</div>
                 </button>
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -3,7 +3,10 @@ import Side_bar from "../../components/Sidebar/inde";
 const Home = () => {
     return (
         <>
-            <Side_bar/>
+            <div className="bg-primario w-screen h-screen flex">
+                <Side_bar className="w-2/12"></Side_bar>/
+                <div className="w-11/12 h-full">container</div>
+            </div>
         </>
     );
 };
