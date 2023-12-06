bf71656fdaab1e60939216486954e60ff5de85e1
CODE-G-ROOT <juandavidavilaravelo856@gmail.com>
2023-12-06 12:29:56 -0500

    feat: :sparkles: Componente De Autores
    se creó y probó el componente de autores, el cual consume la api

---
+++ src/components/Author/Author.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+const Author = ({ url }) => {
+    const [datos, setDatos] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(url)
+                const data = await res.json();
+                setDatos(data)
+            } catch (error) {
+                throw error
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            {datos ? (
+                <>
+                    <div  className="w-full h-full flex justify-between ">
+                        <div className="w-2/3 flex justify-start gap-5">
+                            <div className="w-1/5 h-full flex justify-start items-center">
+                                <img
+                                    className="object-cover w-[4rem] h-[4rem] rounded-2xl"
+                                    src={datos.image}
+                                />
+                            </div>
+                            <div className="w-3/5 flex flex-col justify-center items-start">
+                                <p className="font-medium text-lg">{datos.name}</p>
+                                <span className="text-sm opacity-40">{datos.origin.name}</span>
+                            </div>
+                        </div >
+
+                        <div className="h-full flex justify-end">
+                            <div className="flex gap-3 items-center">
+                                <div className="flex justify-start">
+                                    <span className=" text-base opacity-40">{datos.status}</span>
+                                </div>
+                                <div className=" flex justify-end items-center">
+                                    <div className="text-3xl">{datos.id}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div>Cargando</div>
+            )}
+        </>
+    )
+};
+
+export default Author;
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -3,9 +3,13 @@ import Side_bar from "../../components/Sidebar/Sidebar";
 import Card from "../../components/Card/Card";
 import Card_profile from "../../components/Card/Card_profile";
 import Banner from "../../components/Banner/Banner";
+import Block_Card from "../../components/Card/Bloque_Card";
+import Author from "../../components/Author/Author";
 
 const location = "https://rickandmortyapi.com/api/location/1";
-const personaje = "https://rickandmortyapi.com/api/character/38";
+const personaje = "https://rickandmortyapi.com/api/character/1";
+const personaje2 = "https://rickandmortyapi.com/api/character/2";
+const personaje3= "https://rickandmortyapi.com/api/character/3";
 
 
 //* esta variable se hizo con el objeto de "simular" el consumo de la api, 
@@ -17,12 +21,15 @@ const image = "https://img.freepik.com/vector-premium/planeta-tierra-luna-espaci
 const Home = () => {
     return (
         <>
-            {/* <div className="bg-primario w-screen h-screen flex">
-            <Side_bar className="w-2/12"></Side_bar> */}
-            {/* <Profile/> */}
-            {/* <Card location={location} personaje={personaje} img={image}></Card> */}
-            
-            {/* </div> */}
+            <div className="bg-[#fff] w-screen h-screen flex">
+                {/* <Side_bar className="w-2/12"></Side_bar> */}
+                {/* <Card location={location} personaje={personaje} img={image}></Card> */}
+                <div className="bg-[#fff] w-3/12 h-[5rem] m-1 divide-white-300 divide-y-1">
+                    <Author url={personaje} />
+                    <Author url={personaje2} />
+                    <Author url={personaje3} />
+                </div>
+            </div>
         </>
     );
 };
