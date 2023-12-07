8990841e32b0b7e7738cd346b93110fa850f51a1
CODE-G-ROOT <juandavidavilaravelo856@gmail.com>
2023-12-06 22:20:27 -0500

    feat: :sparkles: Primer Banner implementado
    se ha incorporado el panel "Crear Borrador"

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,9 @@
-const Background_banner = ({ color, img, children }) => {
+const Banner = ({ color, img, children }) => {
     return (
         <>
             <div className={`${color} flex justify-between rounded-3xl`}>
-                <div className="px-14 pt-20 pb-40 text-white-50 w-2/3 flex flex-col justify-between">
-                    <h1 className="text-5xl leading-tight font-normal">
+                <div className="px-14 pt-10 pb-8 text-white-50 w-2/3 flex flex-col justify-between">
+                    <h1 className="text-3xl leading-tight font-normal">
                         Andrés nos gusta tu contenido y nos gustaría darte los siguientes tips
                     </h1>
                     <div className="py-5 ">
@@ -23,38 +23,9 @@ const Background_banner = ({ color, img, children }) => {
 };
 
 
-const colors_bg = ["bg-primario", "bg-secundario", "bg-terciario"];
-const imgs = [
-    "https://fastidious-bookkeeping-services.odoo.com/web_editor/shape/theme_bookstore/s_image_text.svg?c1=o-color-1",
-    "../../../public/presaber11.svg"
-];
+
 
 
 // esto de debe poner en el App, 
-//! NO PUEDE QUEDAR POR AHÍ VOLANDO!!!
-const Banner = () => {
-    return (
-        <>
-            <Background_banner
-                color={colors_bg[1]}
-                img={imgs[0]}
-                className=""
-            >
-
-                añlskdfjaslkdf
-            </Background_banner>
-
-
-            <Background_banner
-                color={colors_bg[2]}
-                img={imgs[1]}
-                className=""
-            >
-
-                <button className="bg-white-50">asdfasdf</button>
-            </Background_banner>
-        </>
-    )
-};
 
 export default Banner;
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -20,6 +20,10 @@ const personaje3 = "https://rickandmortyapi.com/api/character/3";
 
 const image = "https://get.wallhere.com/photo/illustration-planet-space-Earth-cartoon-atmosphere-Rick-and-Morty-universe-astronomy-Adult-Swim-screenshot-atmosphere-of-earth-outer-space-astronomical-object-105144.png";
 
+const imgs_banner = [
+    "https://fastidious-bookkeeping-services.odoo.com/web_editor/shape/theme_bookstore/s_image_text.svg?c1=o-color-1",
+    "../../../public/presaber11.svg"
+];
 
 const Home = () => {
     return (
@@ -29,10 +33,38 @@ const Home = () => {
 
                 <div className="w-5/6 h-full bg-[#fff] rounded-l-3xl">
                     <div className="w-full h-full z-50 bg-[#fff] rounded-l-3xl flex flex-col items-end">
+
                         <NavBar url={personaje}></NavBar>
 
-                        <div className="w-1/4 h-full flex flex-col bg-gray-200 rounded-l-xl">
-                            <TheLast location={location} personaje={personaje} image={image}></TheLast>
+                        <div className="w-full h-full flex">
+                            <div className="w-3/4 h-full">
+                                <div className="p-5 flex flex-col h-full w-full">
+                                    <div className="h-1/5 w-100">
+                                        <Banner color="bg-secundario" img={imgs_banner[0]} >
+                                            <div className="flex flex-col gap-2">
+                                                <div>
+                                                    <p>Vuelve tus libros tendencia mundial!</p>
+                                                    <p>¿Quieres empezar a escribir una vez más?</p>
+                                                </div>
+
+                                                <div className="w-4/12 h-auto bg-terciario flex justify-center gap-5 px-3 py-1 rounded-xl items-center">
+                                                    <p>Crear Borrador </p>
+                                                    <span className="font-extrabold text-3xl">+</span>
+                                                </div>
+                                            </div>
+                                        </Banner>
+                                    </div>
+                                    {/* <div className="h-3/5 w-100">
+                                    </div>
+                                    <div className="h-1/5 w-100">
+                                        <Banner color="bg-terciario" img={imgs_banner[1]} />
+                                    </div> */}
+                                </div>
+
+                            </div>
+                            <div className="w-1/4 h-full flex flex-col bg-terciario/5 rounded-l-3xl shadow-xl shadow-gray-500/40">
+                                <TheLast location={location} personaje={personaje} image={image}></TheLast>
+                            </div>
                         </div>
                     </div>
 
